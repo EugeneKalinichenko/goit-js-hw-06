02a14cab1d9188c376de5c4af762ef6e3a64228e
EugeneKalinichenko <e.o.kalinichenko1994@gmail.com>
2022-01-14 20:10:27 +0200

    Complete 1-t task

---
+++ js/task-01.js
@@ -0,0 +1,31 @@
+const quantityCategories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${quantityCategories.length}`)
+
+quantityCategories.forEach(elem => {
+
+    const firstChildTitle = elem.firstElementChild.textContent;
+    console.log(`Category: ${firstChildTitle}`)
+
+    const quantityChildLastElem = elem.querySelectorAll('li')
+    console.log(`Elements: ${quantityChildLastElem.length}`)
+    
+})
+
+
+
+// Напиши скрипт который:
+
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
